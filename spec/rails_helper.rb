ee0d046e03794a76abd047401b7795813156e1eb
Charlotte Moore <mooc123@gmail.com>
2017-02-08 15:03:16 -0700

    adds user functionality through omniauth and spotify

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'capybara/rails'
 require 'database_cleaner'
 # require 'database_cleaner'
 
+  OmniAuth.config.test_mode = true
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
